d68d8e338148ba2c50058b0c44b140d4cf01e080
Jakub Zalas <jakub@zalas.pl>
2021-06-18 18:21:34 +0200

    Allow to override the kotlin version with gradle properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,10 @@
  */
 
 rootProject.name = "gradle-structurizr-cli"
+
+pluginManagement {
+  val kotlinVersion: String by settings
+  plugins {
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+  }
+}
